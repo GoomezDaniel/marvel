5a1656916a03fa2157c9a90d0ff984140fcc7c6b
Daniel Gomez <dgomez@i-nuba.com>
2022-06-13 18:07:24 +0200

    Implemented blend mode

--- lib/src/commons/background.dart
+++ lib/src/commons/background.dart
@@ -9,19 +9,14 @@ class Background extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Image.asset(
-        image,
-        width: double.infinity,
-        height: double.infinity,
-        fit: BoxFit.cover,
-      ),
-      Container(
-        width: screenWidth(),
-        height: screenHeight(),
-        color: Colors.black.withOpacity(0.75),
-      ),
-    ]);
+    return Image.asset(
+      image,
+      width: double.infinity,
+      height: double.infinity,
+      fit: BoxFit.cover,
+      colorBlendMode: BlendMode.darken,
+      color: Colors.black.withOpacity(0.75),
+    );
   }
 }
 
@@ -32,18 +27,13 @@ class BackgroundNetwork extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Image.network(
-        image,
-        width: double.infinity,
-        height: double.infinity,
-        fit: BoxFit.cover,
-      ),
-      Container(
-        width: screenWidth(),
-        height: screenHeight(),
-        color: Colors.black.withOpacity(0.8),
-      ),
-    ]);
+    return Image.network(
+      image,
+      width: double.infinity,
+      height: double.infinity,
+      fit: BoxFit.cover,
+      colorBlendMode: BlendMode.darken,
+      color: Colors.black.withOpacity(0.8),
+    );
   }
 }
